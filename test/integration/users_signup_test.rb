a5c3e58f1af345a8085325ece535d4114cb41c49
usman2994 <usman.malik2994@gmail.com>
2017-09-07 14:21:41 +0500

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/new'
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
